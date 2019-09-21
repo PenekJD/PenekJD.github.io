c594f4a0c1bf67a3217fda4f1ddc8041ee90abc5
PenekJD <49363681+PenekJD@users.noreply.github.com>
2019-09-21 17:36:21 +0300

    111

--- src/js_frame_1.js
+++ src/js_frame_1.js
@@ -1,6 +1,6 @@
 _PUBLIC_MAIN_LOADER_HTML_DOC_ = `
-    <div style="background:rgba(0,0,0,0.7); color:#fff; font-weight:bold; text-shadow: 1px 1px 1px rgba(0,0,0,0.7); position:absolute; top:0px; left:0px; width:100%;" align="center">
+    <div style="background:rgba(0,0,0,0.7); color:#fff; font-weight:bold; text-shadow: 1px 1px 1px rgba(0,0,0,0.7); position:absolute; top:0px; left:0px; width:100%; z-index: 10;" align="center">
         <div style="padding:15px;">Hello, man!</div>
     </div>
 `;
-GENERATE_EXTRA_HTML_DOC();
+GENERATE_EXTRA_HTML_DOC('MAIN_PUBLIC_ID_1');
